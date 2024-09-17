d92bccc2f3041ca33a2d06b1d774e6ede5a9d939
unknown <you@example.com>
2024-09-17 14:09:48 +0300

    Eddit view link was missing the post param

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <h1>Edit post</h1>
 
-    <form action="{{ route('posts.update') }}" method="post">
+    <form action="{{ route('posts.update', $post) }}" method="post">
         @csrf
         @method('put')
 
         <label for="title">Title: </label>
-        <input type="text" id="title" name="title">
+        <input type="text" id="title" name="title" value="{{ $post->title}}">
         <br>
         <label for="content">Content: </label>
         <textarea name="content" id="content">{{ $post->content }}</textarea>
