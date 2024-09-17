bd69269425626c30fdbb2024c6bfbb3eb92c78b0
unknown <you@example.com>
2024-09-17 13:55:40 +0300

    Corrected route in create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <h1>Create post</h1>
 
-    <form action="{{ route('posts') }}" method="post">
+    <form action="{{ route('posts.store') }}" method="post">
         @csrf
         <label for="title">Title: </label>
         <input type="text" id="title" name="title">
